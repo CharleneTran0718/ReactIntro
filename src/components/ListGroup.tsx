95d529c7a753e144e79b971ef05660964e15bbd0
CharleneTran0718 <charlenehanhtran@gmail.com>
2024-01-31 21:10:25 -0800

    List selecting

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "Korea", "Los Angeles", "California", "Toyko"];
+
+  //Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
             key={item}
-            onClick={() => console.log("Clicked")}
+            onClick={() => { setSelectedIndex(index);}}
           >
             {item}
           </li>
